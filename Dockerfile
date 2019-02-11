0d4baa431f42941c64b58b91f9d62cc2615ec2b0
Johnzell Wilson <jwilson@hurricanelabs.com>
2019-02-11 16:59:33 -0500

    init

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM sonarqube:7.6-community
+
+# Install Custom Plugins
+RUN wget "https://github.com/sbaudoin/sonar-yaml/releases/download/v1.4.0/sonar-yaml-plugin-1.4.0.jar" && \
+    wget "https://github.com/sbaudoin/sonar-ansible/releases/download/v2.0.0/sonar-ansible-plugin-2.0.0.jar" && \
+    wget "https://github.com/QualInsight/qualinsight-plugins-sonarqube-smell/releases/download/qualinsight-plugins-sonarqube-smell-4.0.0/qualinsight-sonarqube-smell-plugin-4.0.0.jar" && \
+    wget "https://github.com/sbaudoin/sonar-shellcheck/releases/download/v2.0.0/sonar-shellcheck-plugin-2.0.0.jar" && \
+    wget "https://github.com/gabrie-allaigre/sonar-gitlab-plugin/releases/download/4.0.0/sonar-gitlab-plugin-4.0.0.jar"
+
+# Replace Already Installed plugins with latest
+RUN rm $SONARQUBE_HOME/extensions/plugins/sonar-java-plugin-* && \
+    rm $SONARQUBE_HOME/extensions/plugins/sonar-python-plugin-* && \
+    wget "https://binaries.sonarsource.com/Distribution/sonar-java-plugin/sonar-java-plugin-5.10.2.17019.jar" && \
+    wget "https://binaries.sonarsource.com/Distribution/sonar-python-plugin/sonar-python-plugin-1.12.0.2726.jar"
+
+# Move Plugins and list plugins
+RUN mv *.jar $SONARQUBE_HOME/extensions/plugins && \
+    ls -lah $SONARQUBE_HOME/extensions/plugins
+
+# Configure LDAP via entrypoint script
+COPY entrypoint.sh $SONARQUBE_HOME/bin/
+USER sonarqube
+ENTRYPOINT ["./bin/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ "${1:0:1}" != '-' ]]; then
+  exec "$@"
+fi
+
+# Configure LDAP
+if [[ ! -z ${LDAP_URL} ]] && [[ ! -z ${LDAP_PORT} ]]; then
+    echo -e "ldap.url=$LDAP_URL:$LDAP_PORT" >> conf/sonar.properties
+    echo -e "sonar.security.realm=LDAP" >> conf/sonar.properties
+fi
+if [[ ! -z ${LDAP_BIND_DN} ]]; then
+    echo -e "ldap.bindDn=$LDAP_BIND_DN" >> conf/sonar.properties
+fi
+if [[ ! -z ${LDAP_PASSWORD} ]]; then
+    echo -e "ldap.bindPassword=$LDAP_PASSWORD" >> conf/sonar.properties
+fi
+if [[ ! -z ${DOMAIN_NAME} ]]; then
+    echo -e "ldap.realm=$DOMAIN_NAME" >> conf/sonar.properties
+fi
+if [[ ! -z ${LDAP_BASE_DN} ]]; then
+    echo -e "ldap.user.baseDn=$LDAP_BASE_DN" >> conf/sonar.properties
+fi
+if [[ ! -z ${LDAP_GROUP_BASE_DN} ]]; then
+    echo -e "ldap.group.base.dn=$LDAP_GROUP_BASE_DN" >> conf/sonar.properties
+fi
+
+
+# Parse Docker env vars to customize SonarQube
+#
+# e.g. Setting the env var sonar.jdbc.username=foo
+#
+# will cause SonarQube to be invoked with -Dsonar.jdbc.username=foo
+
+declare -a sq_opts
+
+while IFS='=' read -r envvar_key envvar_value
+do
+    if [[ "$envvar_key" =~ sonar.* ]]; then
+        sq_opts+=("-D${envvar_key}=${envvar_value}")
+    fi
+done < <(env)
+
+exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
+  -Dsonar.log.console=true \
+  -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
+  -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
+  -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
+  -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
+  "${sq_opts[@]}" \
+  "$@"
