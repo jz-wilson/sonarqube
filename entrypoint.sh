0b456c46b7f03e8b48533015d17bb241343d036a
Johnzell Wilson <jwilson@hurricanelabs.com>
2019-08-20 12:42:14 -0400

    prevent elasticsearch mem check

--- entrypoint.sh
+++ entrypoint.sh
@@ -54,6 +54,7 @@ exec java -jar lib/sonar-application-"$SONAR_VERSION".jar \
   -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
   -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
   -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
-  -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Dnode.store.allow_mmapfs=false -Djava.security.egd=file:/dev/./urandom" \
+  -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
+  -Dsonar.search.javaAdditionalOpts="-Dnode.store.allow_mmapfs=false" \
   "${sq_opts[@]}" \
   "$@"
